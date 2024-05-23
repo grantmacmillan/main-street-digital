f3fa481583ef7382893340a0d0ead29fc4da4703
Grant <grant.macmillan94@gmail.com>
2024-05-23 02:13:11 -0400

    added SEO to blog posts

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,11 +1,57 @@
 import { fetchBlogPosts } from '../../../utils/fetchBlogPosts';
 import { marked } from 'marked';
 
-export default async function BlogPostPage({ params }) {
-    const { slug } = params;
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+
+// Helper function to fetch post data and metadata
+async function getPostData(slug) {
     const posts = await fetchBlogPosts();
     const post = posts.find((post) => post.slug === slug);
-    //console.log('Fetched post:', post); // Log the post to ensure it is fetched
+
+    if (!post) {
+        return {
+            post: null,
+            metadata: {
+                title: 'Post not found',
+                description: 'The post you are looking for does not exist.',
+            }
+        };
+    }
+
+    const metadata = {
+        title: post.title,
+        description: post.description,
+        keywords: post.tags.join(', '),
+        openGraph: {
+            title: post.title,
+            description: post.description,
+            type: 'article',
+            url: `${BASE_URL}/blog/${post.slug}`,
+
+        },
+        twitter: {
+            title: post.title,
+            description: post.description,
+        },
+        alternates: {
+            canonical: `${BASE_URL}/blog/${post.slug}`
+        }
+    };
+
+    return { post, metadata };
+}
+
+// Export metadata function
+export async function generateMetadata({ params }) {
+    const { metadata } = await getPostData(params.slug);
+    return metadata;
+}
+
+export default async function BlogPostPage({ params }) {
+    const { post } = await getPostData(params.slug);
+    console.log('Fetched post:', post); // Log the post to ensure it is fetched
 
     if (!post) {
         return <div>Post not found</div>;
