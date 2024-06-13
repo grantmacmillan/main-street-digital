4dd342fe12b7fa75b87b7400a20fca23d7a12609
Grant <grant.macmillan94@gmail.com>
2024-06-12 20:04:48 -0400

    netlify bug fix

--- app/api/upimage/route.js
+++ app/api/upimage/route.js
@@ -15,25 +15,29 @@ const transporter = nodemailer.createTransport({
 
 console.log('Sending email as:', process.env.GMAIL_USER);
 
-
-
-const pathDist = path.join(process.cwd(), '/public/images');
+const pathDist = '/tmp'; // Use /tmp for temporary file storage in serverless environments
 
 export async function POST(req) {
     try {
+        console.log('Handling form submission');
         const formData = await req.formData();
         const file = formData.get('image');
         const name = formData.get('name');
         const email = formData.get('email');
         const message = formData.get('message');
 
+        if (!file) {
+            throw new Error('File not provided');
+        }
+
         const saveTo = path.join(pathDist, file.name);
         await fs.promises.writeFile(saveTo, Buffer.from(await file.arrayBuffer()));
+        console.log('File saved to:', saveTo);
 
         const mailOptions = {
-            from: process.env.GMAIL_USER, // The email that sends the message
-            to: process.env.GMAIL_USER, // The email that receives the message
-            replyTo: email, // The user's email
+            from: process.env.GMAIL_USER,
+            to: process.env.GMAIL_USER,
+            replyTo: email,
             subject: `New contact form submission from ${name}`,
             text: `You have received a new message from your website contact form.\n\n` +
                 `Here are the details:\n\n` +
@@ -49,13 +53,15 @@ export async function POST(req) {
         };
 
         await transporter.sendMail(mailOptions);
+        console.log('Email sent successfully');
 
         // Remove the file after sending the email
         await fs.promises.unlink(saveTo);
+        console.log('File removed after email sent');
 
         return NextResponse.json({ message: 'Submission successful' });
     } catch (error) {
-        console.error('Error sending email:', error); // Log the error to the console
+        console.error('Error processing submission:', error); // Log the error to the console
         return NextResponse.json({ error: 'Error sending email' }, { status: 500 });
     }
 }
