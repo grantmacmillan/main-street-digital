03e82ae9234eb7e161864a1b3f7cf658eb93ec39
Grant <grant.macmillan94@gmail.com>
2024-06-13 18:40:58 -0400

    fixed robots

--- app/robots.js
+++ app/robots.js
@@ -7,6 +7,6 @@ export default function robots() {
             allow: '/',
             disallow: '/private/',
         },
-        sitemap: '${baseUrl}/sitemap.xml',
+        sitemap: `${baseUrl}/sitemap.js`,
     }
 }
